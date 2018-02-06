8e30779588a1ebb1b29fe86bc1f30d4b60ddc759
talud08 <fjvicen@gmail.com>
2018-02-06 21:38:58 +0100

    nuevo fichero: adios_mundo.php

---
+++ adios_mundo.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hasta pronto Mundo</title>
+    </head>
+    <body>
+        <?php echo '<p>Adios Mundo</p>'; ?>
+        <br />
+        <br />
+        <hr>
+        Nos veremos pronto.
+    </body>
+</html>
